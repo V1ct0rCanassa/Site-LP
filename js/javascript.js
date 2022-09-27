237baf8533880c2040ed8ce2954f3f48d564b809
Vpn210 <103062205+V1ct0rCanassa@users.noreply.github.com>
2022-09-27 19:53:46 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,28 @@
-let count = 1;
-document.getElementById("radio1").checked = true;
+function updateClock(){
+    var now = new Date();
+    var dname = now.getDay(),
+        no = now.getMonth(),
+        dnum = now.getDate(),
+        yr = now.getFullYear(),
+        hou = now.getHours(),
+        min = now.getMinutes(),
+        sec = now.getSeconds();
+    
+        Number.prototype.pad = function(digits){
+            for(var n = this.toString(); n.length < digits; n = 0 + n);
+            return n;
+        }
 
-setInterval( function(){
-    nextImage();
-},5000)
+    var months = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+    var week = ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"];
+    var ids =  ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds"];
+    var values = [week[dname], months[no], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2)];
+    for(var i = 0; i < ids.length; i++)
+    document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+}
 
-function nextImage(){
-    count++;
-    if(count>4){
-        count = 1;
-    }
 
-    document.getElementById("radio"+count).checked = true;
+function initClock(){
+    updateClock();
+    window.setInterval("updateClock()", 1);
 }
